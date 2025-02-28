b8aaed0db73afff02de9e1246d7306cf9eeaf0ff
viveknath3222 <138550507+viveknath3222@users.noreply.github.com>
2025-02-28 14:16:12 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// const heading = React.createElement(
+//     "h1", 
+//     {id: "title"}, 
+//     "Hello from React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        [React.createElement(
+            "h1",
+            {},
+            "Title"
+        ), 
+        React.createElement(
+            "h1",
+            {},
+            "Title1"
+        )]
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
